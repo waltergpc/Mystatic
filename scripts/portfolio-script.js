b39687884d896cb390439fd2ceb7358d0a7a4184
Walter González-Plata Cortes <waltergpc@hotmail.com>
2022-06-04 21:26:32 -0500

    full functionality as previous one with hardcoded HTML

--- scripts/portfolio-script.js
+++ scripts/portfolio-script.js
@@ -7,9 +7,11 @@ const projects = [
     short_description: 'React/MERN Project',
     description:
       'Front-end created in React, with react router v6, API with Node/Express connected to MongoDB Hosted in Atlas. Implementes auth services via HTTP only cookies with access and refresh tokens. Users have roles and based on that back-end and front-end give permissions for data retrieval and operations. Tickets can be submitted and a user can comment on each one. Leaders and admin can assign tickets and users to a team. Charts are created with fusion charts library. Makes use of Cloudinary for image upload in user update and  ticket creation.',
-    websiteURL: 'https://scarabio.netlify.app',
-    front_repo: 'https://github.com/waltergpc/Bug-trakcer-app',
-    back_repo: 'https://github.com/waltergpc/E-commerce-course-API',
+    sources: {
+      Website: 'https://scarabio.netlify.app',
+      'Front-end Repo': 'https://github.com/waltergpc/Bug-trakcer-app',
+      'Back-end Repo': 'https://github.com/waltergpc/E-commerce-course-API',
+    },
   },
   {
     title: 'Bike Terrain Shop',
@@ -19,9 +21,11 @@ const projects = [
     short_description: 'React/MERN Project',
     description:
       'MERN app where a fictional bike store is showcased. This app was developed with React and react router v6, also it implements some netlify serverless functions in order to set payments with stripe. Authentication services are implemented through Auth0. Product list and filtering is made throughout context APIs. API was developed with Node and express, where a simple API was implemented to supply the stock and specs for each bike, cart functionality cannot exceed the numbers of the stock supplied by the API.',
-    websiteURL: 'https://bike-terrain-shop.netlify.app',
-    front_repo: 'https://github.com/waltergpc/BIke-shop-app',
-    back_repo: 'https://github.com/waltergpc/BIKE-API',
+    sources: {
+      Website: 'https://bike-terrain-shop.netlify.app',
+      'Front-end Repo': 'https://github.com/waltergpc/BIke-shop-app',
+      'Back-end Repo': 'https://github.com/waltergpc/BIKE-API',
+    },
   },
   {
     title: 'Inventory App',
@@ -31,9 +35,11 @@ const projects = [
     short_description: 'React/MERN Project',
     description:
       'This MERN app was created using React and react router v6, it sets an inventory for an office environment and list them as general and personal supplies. the front-end was developed using React context API for state management. The API was developed with express where authentication with JWT is implemented and then routes are provided for the app to get the items for each user, it is lnked to a Mongo database where the users and items are stored.',
-    websiteURL: 'https://silver-inventory.netlify.app',
-    front_repo: 'https://github.com/waltergpc/office-inventory-app',
-    back_repo: 'https://github.com/waltergpc/Inventory_API',
+    sources: {
+      Website: 'https://silver-inventory.netlify.app',
+      'Front-end Repo': 'https://github.com/waltergpc/office-inventory-app',
+      'Back-end Repo': 'https://github.com/waltergpc/Inventory_API',
+    },
   },
   {
     title: 'Cryptocurrency tracker',
@@ -43,9 +49,10 @@ const projects = [
     short_description: 'React Project',
     description:
       'This React App is a cryptocurrency tracker where a central component is mounted with price and market data for each project, in the central component pagination is introduced to show up to 100 projects per page and a max of 400 projects. The single coin component showcases components where data is acheived via visual communication, like color for % changes, tooltips for information and a d3-onlñy chart for a 3 month price follow-up.',
-    websiteURL: 'https://silver-tracker.netlify.app',
-    front_repo: 'https://github.com/DPLATA/crypto_tracker',
-    back_repo: '',
+    sources: {
+      Website: 'https://silver-tracker.netlify.app',
+      'Front-end Repo': 'https://github.com/DPLATA/crypto_tracker',
+    },
   },
 ]
 
@@ -54,7 +61,7 @@ const container = document.querySelector('#projects-container')
 console.log(projects)
 
 projects.forEach((project) => {
-  const { title, img, intro, short_description, description } = project
+  const { title, img, intro, short_description, description, sources } = project
   let newProject = document.createElement('article')
   newProject.classList.add('project')
   newProject.innerHTML = `
@@ -69,6 +76,17 @@ projects.forEach((project) => {
   </div>
 </div>
 <hr />`
+
+  const projectSources = document.createElement('div')
+  projectSources.classList.add('sources')
+  for (const source in sources) {
+    const sourceSpan = document.createElement('span')
+    sourceSpan.classList.add('source-item')
+    sourceSpan.innerHTML = `<a href=${sources[source]}>${source}</a></span>`
+    projectSources.appendChild(sourceSpan)
+  }
+
+  newProject.appendChild(projectSources)
 
   container.appendChild(newProject)
 })
